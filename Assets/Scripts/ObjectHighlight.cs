fe734a3725948937fe30a90cbff7f7f4838b773b
ManChildMan <rocvig92@hotmail.com>
2015-10-10 15:49:17 +0800

    Combat Update
    - Added Highlight function

---
+++ Assets/Scripts/ObjectHighlight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHighlight : MonoBehaviour {
+
+    public string objectName;
+
+    private Color startColour;
+    private bool _displayObjectName;
+
+    void OnGUI()
+    {
+        DisplayName();
+    }
+
+    void OnMouseEnter()
+    {
+        startColour = GetComponentInChildren<Renderer>().material.color;
+        GetComponentInChildren<Renderer>().material.color = Color.blue;
+        _displayObjectName = true;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponentInChildren<Renderer>().material.color = startColour;
+        _displayObjectName = false;
+    }
+
+    public void DisplayName()
+    {
+        if (_displayObjectName == true)
+        {
+            GUI.Box(new Rect(Event.current.mousePosition.x - 155, Event.current.mousePosition.y, 150, 25), objectName);
+        }
+    }
+}
