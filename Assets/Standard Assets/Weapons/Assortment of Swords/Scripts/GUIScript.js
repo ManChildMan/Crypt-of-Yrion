ae004b8c5b7173ad90edbff507b16a48b9a44213
ManChildMan <rocvig92@hotmail.com>
2015-10-19 17:54:41 +0800

    Combat Update
    - Added Weapons

---
+++ Assets/Standard Assets/Weapons/Assortment of Swords/Scripts/GUIScript.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+var weapons : GameObject[];
+
+function NextWeapon() {
+	if(weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+}
+
+function PreviousWeapon() {
+	if(weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[0].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[1].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[2].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+	else if(weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled == true) {
+		weapons[4].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = false;
+		weapons[3].GetComponent.<MeshRenderer>().GetComponent.<Renderer>().enabled = true;
+	}
+}
---
+++ Assets/Standard Assets/Weapons/Assortment of Swords/Scripts/RotationScript.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+var rotSpeed : float = 2;
+
+function FixedUpdate () {
+	transform.Rotate(0, 0, rotSpeed);
+}
