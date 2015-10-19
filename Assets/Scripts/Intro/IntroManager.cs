eb9a7501c86976b403dce1c02b80e57733823bfe
Jordan Singh <xanather@hotmail.com>
2015-10-19 14:33:23 +0800

    Push basic intro scene

---
+++ Assets/Scripts/Intro/IntroManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class IntroManager : MonoBehaviour
+{
+    // References set by unity.
+    public GameObject textObject;
+    public GameObject nextButtonObject;
+    public string[] strings;
+
+    private Text text;
+    private Text nextButtonText;
+
+    private int index;
+    private int indexWithinString;
+    private bool showing;
+    private const float showLetterTimerTotal = 0.05f; // Show a letter every 1/10th of a second (100 milliseconds).
+    private float showLetterTimer; 
+
+	void Start ()
+    {
+        text = textObject.GetComponent<Text>();
+        nextButtonText = nextButtonObject.transform.FindChild("Text").gameObject.GetComponent<Text>();
+        showing = true;
+    }
+	
+	void Update ()
+    {
+        if (showing)
+        {
+            showLetterTimer += Time.deltaTime;
+            while (showLetterTimer >= showLetterTimerTotal)
+            {
+                showLetterTimer -= showLetterTimerTotal;
+                indexWithinString++;
+                text.text = strings[index].Substring(0, indexWithinString);
+                if (indexWithinString == strings[index].Length)
+                {
+                    showing = false;
+                    nextButtonObject.SetActive(true);
+                    if (index == strings.Length - 1)
+                    {
+                        nextButtonText.text = "Enter the Crypt";
+                    }
+                }
+            }
+        }
+	}
+
+    public void NextLine()
+    {
+        if (index < strings.Length - 1)
+        {
+            showLetterTimer = 0.0f;
+            text.text = "";
+            showing = true;
+            index++;
+            indexWithinString = 0;
+            nextButtonObject.SetActive(false);
+        }
+        else
+        {
+            Application.LoadLevel("safezone");
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    // References set by unity.
+    public GameObject transitionImageObject;
+    private Image transitionImage;
+
+    private bool transition;
+    private ToState toState;
+    private bool prevTransition;
+    private float transitionRatio;
+
+    private enum ToState
+    {
+        NewGame,
+        LoadGame,
+        Quit
+    }
+
+    void Start()
+    {
+        transitionImage = transitionImageObject.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (prevTransition != transition)
+        {
+            prevTransition = transition;
+            transitionImageObject.SetActive(true);
+            transitionImage.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+        if (transition)
+        {
+            transitionRatio += Time.deltaTime;
+            if (transitionRatio >= 1.0f)
+            {
+                switch (toState)
+                {
+                    case ToState.NewGame:
+                        Application.LoadLevel("intro");
+                        break;
+                    case ToState.LoadGame:
+                        break;
+                    case ToState.Quit:
+                        Application.Quit();
+                        // Application.Quit() does not work when running from Unity Editor, so just reshow everything after the transition effect.
+                        transition = false;
+                        prevTransition = false;
+                        transitionImageObject.SetActive(false);
+                        transitionRatio = 0.0f;
+                        break;
+                }
+            }
+            else
+            {
+                transitionImage.color = new Color(0.0f, 0.0f, 0.0f, transitionRatio);
+            }
+        }
+
+    }
+    
+    public void NewGame()
+    {
+        if (transition)
+        {
+            return;
+        }
+        toState = ToState.NewGame;
+        transition = true;
+    }
+    
+    public void LoadGame()
+    {
+        if (transition)
+        {
+            return;
+        }
+        toState = ToState.LoadGame;
+        transition = true;
+    }
+    
+    public void Settings()
+    {
+        if (transition)
+        {
+            return;
+        }
+    }
+    
+    public void Quit()
+    {
+        toState = ToState.Quit;
+        transition = true;
+    }
+}
