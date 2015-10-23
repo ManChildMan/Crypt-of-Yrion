a76a63acfea270f2cb4590b180abc4b3f0cb95a1
Jordan Singh <xanather@hotmail.com>
2015-10-23 14:00:37 +0800

    Change back to using left click on GridSquare (Right clicking was not working well).

--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -22,22 +22,19 @@ public class GridSquare : MonoBehaviour
         m_renderer = GetComponentInChildren<MeshRenderer>();
 	}
 
-    void OnMouseOver()
+    void OnMouseDown()
     {
-        if (Input.GetMouseButtonUp(1))
-        {
-            // Return if already selected.
-            if (IsSelected)
-                return;
+        // Return if already selected.
+        if (IsSelected)
+            return;
 
-            // Set the 'selected' material.
-            m_renderer.material = SelectedMaterial;
-            IsSelected = true;
+        // Set the 'selected' material.
+        m_renderer.material = SelectedMaterial;
+        IsSelected = true;
 
-            // Fire the selected event.
-            if (OnGridSquareSelected != null)
-                OnGridSquareSelected(this);
-        }
+        // Fire the selected event.
+        if (OnGridSquareSelected != null)
+            OnGridSquareSelected(this);
     }
 
     public void Unselect()
